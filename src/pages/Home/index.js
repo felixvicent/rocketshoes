0563ebc2913149afa5550cc68e8c4407b60986c9
Felix Vicente <felixvicent1306@gmail.com>
2020-12-05 15:38:59 -0300

    show qtd products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,6 +37,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -51,7 +52,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />
+                {amount[product.id] || 0}
               </div>
 
               <span>ADCIONAR AO CARRINHO</span>
@@ -65,9 +67,18 @@ class Home extends Component {
 
 Home.propTypes = {
   addToCart: PropTypes.func.isRequired,
+  amount: PropTypes.number.isRequired,
 };
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
