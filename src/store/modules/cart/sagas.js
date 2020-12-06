ae24d4060a3a118fc0b5669e76f940442d090dc7
Felix Vicente <felixvicent1306@gmail.com>
2020-12-06 17:58:48 -0300

    add react-toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import { addToCartSuccess, updateAmount } from './actions';
 import api from '../../../services/api';
@@ -17,7 +18,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn('ERROR');
+    toast.error('Quantidade solicitada fora de estoque');
 
     return;
   }
